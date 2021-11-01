3d9e558b276478f683864adc9b717016d52140d7
simeunseok <ses8323@gmail.com>
2021-11-01 18:49:24 +0900

    Issue-2 feat: CheckPointLocalDataSource 구현

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/checkpoint/local/CheckPointLocalDataSourceImpl.kt
@@ -0,0 +1,26 @@
+package com.drunkenboys.calendarun.data.checkpoint.local
+
+import com.drunkenboys.calendarun.data.checkpoint.entity.CheckPoint
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class CheckPointLocalDataSourceImpl(
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    private val checkPointDao: CheckPointDao
+) : CheckPointLocalDataSource {
+
+    override suspend fun insertCheckPoint(checkPoint: CheckPoint) {
+        withContext(dispatcher) {
+            checkPointDao.insertCheckPoint(checkPoint)
+        }
+    }
+
+    override suspend fun fetchAllCheckPoint() = withContext(dispatcher) {
+        checkPointDao.fetchAllCheckPoint()
+    }
+
+    override suspend fun fetchCheckPoint(id: Int) = withContext(dispatcher) {
+        checkPointDao.fetchCheckPoint(id)
+    }
+}
