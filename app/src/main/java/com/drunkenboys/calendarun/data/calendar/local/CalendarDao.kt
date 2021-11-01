7dbe22ca5276fe5244ea7d57aed8f3cba3f07a89
simeunseok <ses8323@gmail.com>
2021-11-01 17:56:19 +0900

    Issue-2 feat: CalendarDao 생성

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/calendar/local/CalendarDao.kt
@@ -0,0 +1,19 @@
+package com.drunkenboys.calendarun.data.calendar.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.drunkenboys.calendarun.data.calendar.entity.Calendar
+
+@Dao
+interface CalendarDao {
+
+    @Insert
+    suspend fun insertCalendar(calendar: Calendar)
+
+    @Query("SELECT * FROM `calendar`")
+    suspend fun fetchAllCalendar()
+
+    @Query("SELECT * FROM `calendar` WHERE id == :id")
+    suspend fun fetchCalendar(id: Int)
+}
