1ae5e481bc0dfdc9911a547a8697792edf2e748b
simeunseok <ses8323@gmail.com>
2021-11-01 18:42:46 +0900

    Issue-2 feat: CalendarLocalDataSource 구현

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/calendar/local/CalendarLocalDataSourceImpl.kt
@@ -0,0 +1,26 @@
+package com.drunkenboys.calendarun.data.calendar.local
+
+import com.drunkenboys.calendarun.data.calendar.entity.Calendar
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class CalendarLocalDataSourceImpl(
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+    private val calendarDao: CalendarDao
+) : CalendarLocalDataSource {
+
+    override suspend fun insertCalendar(calendar: Calendar) {
+        withContext(dispatcher) {
+            calendarDao.insertCalendar(calendar)
+        }
+    }
+
+    override suspend fun fetchAllCalendar() = withContext(dispatcher) {
+        calendarDao.fetchAllCalendar()
+    }
+
+    override suspend fun fetchCalendar(id: Int) = withContext(dispatcher) {
+        calendarDao.fetchCalendar(id)
+    }
+}
