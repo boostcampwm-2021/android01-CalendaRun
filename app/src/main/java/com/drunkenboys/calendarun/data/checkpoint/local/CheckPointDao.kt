413e303073938ee271bdbfe585e6ca216ee3d3d2
simeunseok <ses8323@gmail.com>
2021-11-01 18:01:04 +0900

    Issue-2 feat: CheckPointDao 생성

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/checkpoint/local/CheckPointDao.kt
@@ -0,0 +1,20 @@
+package com.drunkenboys.calendarun.data.checkpoint.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.drunkenboys.calendarun.data.checkpoint.entity.CheckPoint
+
+@Dao
+interface CheckPointDao {
+
+    @Insert
+    suspend fun insertCheckPoint(checkPoint: CheckPoint)
+
+    @Query("SELECT * FROM `checkpoint`")
+    suspend fun fetchAllCheckPoint()
+
+    @Query("SELECT * FROM `checkpoint` WHERE id == :id")
+    suspend fun fetchCheckPoint(id: Int)
+
+}
