ec4544bf2394c11103d33b8bffaa0366590a5a84
simeunseok <ses8323@gmail.com>
2021-11-01 17:26:29 +0900

    Issue-2 feat: Calendar Entity 생성

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/local/calendar/entity/Calendar.kt
@@ -0,0 +1,13 @@
+package com.drunkenboys.calendarun.data.local.calendar.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
+@Entity(tableName = "calendar")
+data class Calendar(
+    @PrimaryKey val id: Int,
+    val name: String,
+    val startDate: Date,
+    val endDate: Date
+)
