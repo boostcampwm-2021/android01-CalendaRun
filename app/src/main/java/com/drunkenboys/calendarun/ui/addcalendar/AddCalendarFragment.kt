202b4e9d1dce29999e88612a9873985eeee37418
simeunseok <ses8323@gmail.com>
2021-11-03 00:33:05 +0900

    Issue #12 feat: RecyclerView adapter 연결

--- app/src/main/java/com/drunkenboys/calendarun/ui/addcalendar/AddCalendarFragment.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/addcalendar/AddCalendarFragment.kt
@@ -1,4 +1,4 @@
-package com.drunkenboys.calendarun.ui
+package com.drunkenboys.calendarun.ui.addcalendar
 
 import android.app.DatePickerDialog
 import android.os.Bundle
@@ -6,15 +6,23 @@ import android.view.View
 import android.widget.TextView
 import com.drunkenboys.calendarun.R
 import com.drunkenboys.calendarun.databinding.FragmentAddCalendarBinding
+import com.drunkenboys.calendarun.ui.addcalendar.adapter.AddCalendarRecyclerViewAdapter
 import com.drunkenboys.calendarun.ui.base.BaseFragment
 import java.util.*
 
 class AddCalendarFragment : BaseFragment<FragmentAddCalendarBinding>(R.layout.fragment_add_calendar) {
 
+    private val recyclerViewAdapter by lazy { AddCalendarRecyclerViewAdapter() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setRecyclerViewAdapter()
         setTextDatePickerClickListener()
+    }
+
+    private fun setRecyclerViewAdapter() {
+        binding.rAddCalendarCheckPointList.adapter = recyclerViewAdapter
     }
 
     private fun setTextDatePickerClickListener() {
@@ -30,6 +38,7 @@ class AddCalendarFragment : BaseFragment<FragmentAddCalendarBinding>(R.layout.fr
         val month = cal.get(Calendar.MONTH)
         val dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
         val dateSetListener = DatePickerDialog.OnDateSetListener { _, pickYear, pickMonth, pickDayOfMonth ->
+            // TODO ViewModel에 저장하는 방식으로 개선
             textView.text = getString(R.string.ui_date_format, pickYear, pickMonth, pickDayOfMonth)
         }
 
