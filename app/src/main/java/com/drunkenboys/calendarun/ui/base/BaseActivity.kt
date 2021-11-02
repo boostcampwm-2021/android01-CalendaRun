08354e9e8af4bd121dec9f90a77aecb7609d6ec6
Seonghwan Kim <shshksh58@gmail.com>
2021-11-02 15:26:55 +0900

    Issue #10 feat: BaseActivity 추가

---
+++ app/src/main/java/com/drunkenboys/calendarun/ui/base/BaseActivity.kt
@@ -0,0 +1,34 @@
+package com.drunkenboys.calendarun.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.viewbinding.ViewBinding
+
+// for view binding
+open class BaseViewActivity<T : ViewBinding>(
+    private val inflate: ((LayoutInflater) -> T)? = null
+) : AppCompatActivity() {
+
+    lateinit var binding: T
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        inflate?.let {
+            binding = it.invoke(layoutInflater)
+            setContentView(binding.root)
+        }
+    }
+}
+
+// for data binding
+open class BaseActivity<T : ViewDataBinding>(private val layoutId: Int) : BaseViewActivity<T>() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutId)
+        binding.lifecycleOwner = this
+    }
+}
