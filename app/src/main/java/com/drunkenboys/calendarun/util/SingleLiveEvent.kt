fee22d1e90dc279711e77c863a0b8d4f02222612
Seonghwan Kim <shshksh58@gmail.com>
2021-11-03 00:17:44 +0900

    Issue #16 feat: SingleLiveEvent 클래스 추가

---
+++ app/src/main/java/com/drunkenboys/calendarun/util/SingleLiveEvent.kt
@@ -0,0 +1,27 @@
+package com.drunkenboys.calendarun.util
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val mPending: AtomicBoolean = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        super.observe(owner) {
+            if (mPending.compareAndSet(true, false)) {
+                observer.onChanged(it)
+            }
+        }
+    }
+
+    @MainThread
+    override fun setValue(value: T?) {
+        mPending.set(true)
+        super.setValue(value)
+    }
+}
