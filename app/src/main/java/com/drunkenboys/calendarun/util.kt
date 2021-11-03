6d9daf760aece7577cc9b85aee43439eb6730202
simeunseok <ses8323@gmail.com>
2021-11-03 16:14:51 +0900

    Issue #12 refactor: showDatePickerDialog 변경
    View가 아닌 DatePickerDialog.OnDateSetListener를 매개변수로 받도록 변경

--- app/src/main/java/com/drunkenboys/calendarun/util.kt
+++ app/src/main/java/com/drunkenboys/calendarun/util.kt
@@ -1,6 +1,8 @@
 package com.drunkenboys.calendarun
 
 import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.content.Context
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -9,4 +11,13 @@ fun toStringDateFormat(date: Date): String {
     val dateFormat = SimpleDateFormat("yyyy.MM.dd")
 
     return dateFormat.format(date)
+}
+
+fun showDatePickerDialog(context: Context, dateSetListener: DatePickerDialog.OnDateSetListener) {
+    val cal = Calendar.getInstance()
+    val year = cal.get(Calendar.YEAR)
+    val month = cal.get(Calendar.MONTH)
+    val dayOfMonth = cal.get(Calendar.DAY_OF_MONTH)
+
+    DatePickerDialog(context, dateSetListener, year, month, dayOfMonth).show()
 }
