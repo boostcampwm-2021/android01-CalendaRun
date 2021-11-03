dd52dd102ce48fd4de5193fdfc64eea3ad8e6f30
Seonghwan Kim <shshksh58@gmail.com>
2021-11-03 13:35:37 +0900

    Issue #16 feat: notification 옵저버 등록

--- app/src/main/java/com/drunkenboys/calendarun/ui/saveschedule/SaveScheduleFragment.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/saveschedule/SaveScheduleFragment.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.viewModels
 import com.drunkenboys.calendarun.R
 import com.drunkenboys.calendarun.databinding.FragmentSaveScheduleBinding
 import com.drunkenboys.calendarun.ui.base.BaseFragment
+import com.drunkenboys.calendarun.ui.saveschedule.model.ScheduleNotificationType
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,12 +19,25 @@ class SaveScheduleFragment : BaseFragment<FragmentSaveScheduleBinding>(R.layout.
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = viewModel
 
+        observeNotification()
         observeTagColor()
     }
 
+    private fun observeNotification() {
+        viewModel.notification.observe(viewLifecycleOwner) { type ->
+            binding.tvSaveScheduleNotification.text = when (type) {
+                ScheduleNotificationType.NONE -> getString(R.string.notification_none)
+                ScheduleNotificationType.TEN_MINUTES_AGO -> getString(R.string.notification_ten_minutes_ago)
+                ScheduleNotificationType.A_HOUR_AGO -> getString(R.string.notification_a_hour_ago)
+                ScheduleNotificationType.A_DAY_AGO -> getString(R.string.notification_a_day_ago)
+                null -> ""
+            }
+        }
+    }
+
     private fun observeTagColor() {
-        viewModel.tagColor.observe(viewLifecycleOwner) {
-            binding.viewSaveScheduleTagColor.backgroundTintList = ContextCompat.getColorStateList(requireContext(), it)
+        viewModel.tagColor.observe(viewLifecycleOwner) { colorRes ->
+            binding.viewSaveScheduleTagColor.backgroundTintList = ContextCompat.getColorStateList(requireContext(), colorRes)
         }
     }
 }
--- app/src/main/java/com/drunkenboys/calendarun/ui/saveschedule/model/ScheduleNotificationType.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/saveschedule/model/ScheduleNotificationType.kt
@@ -1,8 +1,8 @@
 package com.drunkenboys.calendarun.ui.saveschedule.model
 
-enum class ScheduleNotificationType(val message: String) {
-    NONE("없음"),
-    TEN_MINUTES_AGO("10분 전 알림"),
-    A_HOUR_AGO("한 시간 전 알림"),
-    A_DAY_AGO("하루 전 알림")
+enum class ScheduleNotificationType {
+    NONE,
+    TEN_MINUTES_AGO,
+    A_HOUR_AGO,
+    A_DAY_AGO
 }
