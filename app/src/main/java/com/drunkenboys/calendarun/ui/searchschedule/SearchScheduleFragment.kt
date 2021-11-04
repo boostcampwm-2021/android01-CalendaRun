afe5b8acc49fd8a117bc694a7f91d7ea561bdd66
Seonghwan Kim <shshksh58@gmail.com>
2021-11-05 00:00:09 +0900

    Issue #39 feat: 툴바 백 버튼 색 변경

--- app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleFragment.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleFragment.kt
@@ -1,10 +1,26 @@
 package com.drunkenboys.calendarun.ui.searchschedule
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.drunkenboys.calendarun.R
-import com.drunkenboys.calendarun.databinding.FragmentSaveScheduleBinding
+import com.drunkenboys.calendarun.databinding.FragmentSearchScheduleBinding
 import com.drunkenboys.calendarun.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SearchScheduleFragment : BaseFragment<FragmentSaveScheduleBinding>(R.layout.fragment_search_schedule) {
+class SearchScheduleFragment : BaseFragment<FragmentSearchScheduleBinding>(R.layout.fragment_search_schedule) {
+
+    private val navController by lazy { findNavController() }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupToolbar()
+    }
+
+    private fun setupToolbar() {
+        binding.toolbarSearchSchedule.setupWithNavController(navController)
+    }
 }
