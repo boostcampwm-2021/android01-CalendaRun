a43808da9d6fd184df9f49ee819893157e272d8e
차지원 <chajiwon100785@gmail.com>
2021-11-04 16:20:39 +0900

    Issue #25 feat: parse DayOfWeek -> DayType util에 추가

--- library/src/main/java/com/drunkenboys/ckscalendar/utils/TimeUtils.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/utils/TimeUtils.kt
@@ -1,7 +1,19 @@
 package com.drunkenboys.ckscalendar.utils
 
+import com.drunkenboys.ckscalendar.data.DayType
+import java.time.DayOfWeek
+
 object TimeUtils {
     fun getColorInt(R: Int, G: Int, B: Int): Int {
         return 0xff shl 24 or (R and 0xff shl 16) or (G and 0xff shl 8) or (B and 0xff)
+    }
+
+//    (MonthPageAdapter.kt에서 카피)
+    fun parseDayWeekToDayType(week: DayOfWeek): DayType {
+        return when (week) {
+            DayOfWeek.SATURDAY -> DayType.SATURDAY
+            DayOfWeek.SUNDAY -> DayType.HOLIDAY
+            else -> DayType.WEEKDAY
+        }
     }
 }
