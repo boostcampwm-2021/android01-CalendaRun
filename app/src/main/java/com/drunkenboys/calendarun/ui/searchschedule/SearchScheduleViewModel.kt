8c63ad1d77dae19346d381b730af165779dcb8bb
Seonghwan Kim <shshksh58@gmail.com>
2021-11-05 01:11:15 +0900

    Issue #40 feat: 일정 데이터 fetch 기능 구현

---
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleViewModel.kt
@@ -0,0 +1,41 @@
+package com.drunkenboys.calendarun.ui.searchschedule
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.drunkenboys.calendarun.data.schedule.entity.Schedule
+import com.drunkenboys.calendarun.data.schedule.local.ScheduleLocalDataSource
+import com.drunkenboys.calendarun.ui.searchschedule.model.DateItem
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import java.util.*
+import javax.inject.Inject
+
+@HiltViewModel
+class SearchScheduleViewModel @Inject constructor(
+    private val scheduleDataSource: ScheduleLocalDataSource
+) : ViewModel() {
+
+    private val _listItem = MutableLiveData<List<DateItem>>()
+    val listItem: LiveData<List<DateItem>> = _listItem
+
+    fun fetchScheduleList() {
+        viewModelScope.launch {
+            val today = Date()
+
+            _listItem.value = scheduleDataSource.fetchAllSchedule()
+                .filter { schedule -> schedule.startDate >= today }
+                .groupBy { schedule -> schedule.dayMillis() }
+                .map { (dayMillis, scheduleList) -> DateItem(Date(dayMillis), scheduleList) }
+                .sortedBy { dateItem -> dateItem.date }
+        }
+    }
+
+    private fun Schedule.dayMillis() = Calendar.getInstance().apply {
+        time = this@dayMillis.startDate
+        set(Calendar.HOUR_OF_DAY, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.SECOND, 0)
+    }.timeInMillis
+}
