ca19d15eef47ef2eeba4fb77ddff23a53161601b
Seonghwan Kim <shshksh58@gmail.com>
2021-11-05 00:14:27 +0900

    Issue #39 feat: 페이지 진입 시 IMM show

--- app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleFragment.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleFragment.kt
@@ -2,6 +2,8 @@ package com.drunkenboys.calendarun.ui.searchschedule
 
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.getSystemService
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.drunkenboys.calendarun.R
@@ -22,5 +24,12 @@ class SearchScheduleFragment : BaseFragment<FragmentSearchScheduleBinding>(R.lay
 
     private fun setupToolbar() {
         binding.toolbarSearchSchedule.setupWithNavController(navController)
+        showIMM()
+    }
+
+    private fun showIMM() {
+        binding.etSearchScheduleToolbar.requestFocus()
+        val imm = requireContext().getSystemService<InputMethodManager>()
+        imm?.showSoftInput(binding.etSearchScheduleToolbar, InputMethodManager.SHOW_IMPLICIT)
     }
 }
