912a6dd2d9373c95d980079195b74616002ffbe6
Seonghwan Kim <shshksh58@gmail.com>
2021-11-04 18:16:55 +0900

    Issue #36 feat: MainCalendarFragment 추가

---
+++ app/src/main/java/com/drunkenboys/calendarun/ui/maincalendar/MainCalendarFragment.kt
@@ -0,0 +1,8 @@
+package com.drunkenboys.calendarun.ui.maincalendar
+
+import com.drunkenboys.calendarun.R
+import com.drunkenboys.calendarun.databinding.FragmentMainCalendarBinding
+import com.drunkenboys.calendarun.ui.base.BaseFragment
+
+class MainCalendarFragment : BaseFragment<FragmentMainCalendarBinding>(R.layout.fragment_main_calendar) {
+}
