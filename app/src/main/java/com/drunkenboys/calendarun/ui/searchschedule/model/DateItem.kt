a8ff4a9f7896aaf3a14e8e6d03cd10fb5bb47840
Seonghwan Kim <shshksh58@gmail.com>
2021-11-05 01:49:05 +0900

    Issue #40 feat: DateItem 날짜 포맷팅

--- app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/model/DateItem.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/model/DateItem.kt
@@ -1,9 +1,16 @@
 package com.drunkenboys.calendarun.ui.searchschedule.model
 
 import com.drunkenboys.calendarun.data.schedule.entity.Schedule
+import java.text.SimpleDateFormat
 import java.util.*
 
 data class DateItem(
     val date: Date,
     val schedule: List<Schedule>
-)
+) {
+
+    val dateName: String = run {
+        val df = SimpleDateFormat("M월 d일", Locale.getDefault())
+        df.format(date)
+    }
+}
