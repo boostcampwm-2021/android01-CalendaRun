3f9e37e716a1caaa99b249afb02b582b25b39f22
cha-ji <chajiwon100785@gmail.com>
2021-11-08 19:08:38 +0900

    Issue #48 fix: year calendar textAlign center

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarView.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarView.kt
@@ -3,16 +3,12 @@ package com.drunkenboys.ckscalendar.yearcalendar
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
-import android.view.View
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -20,11 +16,11 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.text.style.TextAlign
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.view.marginStart
 import androidx.databinding.DataBindingUtil
 import com.drunkenboys.ckscalendar.R
 import com.drunkenboys.ckscalendar.databinding.LayoutYearCalendarBinding
@@ -34,7 +30,6 @@ import com.drunkenboys.ckscalendar.data.CalendarSet
 import com.drunkenboys.ckscalendar.data.DayType
 import com.drunkenboys.ckscalendar.utils.TimeUtils
 import java.time.DayOfWeek
-import java.util.*
 
 @ExperimentalAnimationApi
 @ExperimentalFoundationApi
@@ -107,42 +102,49 @@ class YearCalendarView
             top.linkTo(parent.top)
             start.linkTo(parent.start)
             end.linkTo(monday.start)
+            width = Dimension.fillToConstraints
         }
 
         constrain(monday) {
             top.linkTo(parent.top)
             start.linkTo(sunday.end)
             end.linkTo(tuesday.start)
+            width = Dimension.fillToConstraints
         }
 
         constrain(tuesday) {
             top.linkTo(parent.top)
             start.linkTo(monday.end)
             end.linkTo(wednesday.start)
+            width = Dimension.fillToConstraints
         }
 
         constrain(wednesday) {
             top.linkTo(parent.top)
             start.linkTo(tuesday.end)
             end.linkTo(thursday.start)
+            width = Dimension.fillToConstraints
         }
 
         constrain(thursday) {
             top.linkTo(parent.top)
             start.linkTo(wednesday.end)
             end.linkTo(friday.start)
+            width = Dimension.fillToConstraints
         }
 
         constrain(friday) {
             top.linkTo(parent.top)
             start.linkTo(thursday.end)
             end.linkTo(saturday.start)
+            width = Dimension.fillToConstraints
         }
 
         constrain(saturday) {
             top.linkTo(parent.top)
             start.linkTo(friday.end)
             end.linkTo(parent.end)
+            width = Dimension.fillToConstraints
         }
     }
 
@@ -156,6 +158,8 @@ class YearCalendarView
                 stickyHeader {
                     Text( //FIXME: background 투명하지 않게 설정
                         text = "${year[0].startDate.year}년",
+                        modifier = Modifier.background(color = Color.White).fillMaxWidth(),
+                        textAlign = TextAlign.Center
                     )
                 }
 
@@ -172,6 +176,7 @@ class YearCalendarView
                             }
 
                             // 일주일 나열
+                            // TODO: 디자인 설정
                             week.forEach { day ->
                                 if (day.dayType != DayType.PADDING)
                                     Text(
@@ -182,14 +187,17 @@ class YearCalendarView
                                             DayType.SUNDAY -> Color.Red
                                             else -> Color.Black
                                         },
-                                        modifier = Modifier
-                                            .layoutId(day.date.toString())
+                                        modifier = Modifier.layoutId(day.date.toString()),
+                                        textAlign = TextAlign.Center,
                                     )
                                 else
                                     Text(
                                         text = "${day.date.dayOfMonth}",
-                                        modifier = Modifier.layoutId(day.date.toString()).alpha(0f)
+                                        modifier = Modifier.layoutId(day.date.toString()).alpha(0f),
+                                        textAlign = TextAlign.Center,
                                     )
+                                // TODO: 스케줄 탐색
+
                             }
                         }
                     }
@@ -208,7 +216,6 @@ class YearCalendarView
         // 앞쪽 패딩
         if (paddingPrev.dayOfWeek != DayOfWeek.SUNDAY) weekList.add(mutableListOf())
         while (paddingPrev.dayOfWeek != DayOfWeek.SUNDAY) {
-            // FIXME: 없는 날짜로 수정해야 스케줄 탐색 가능
             paddingPrev = paddingPrev.minusDays(1)
             weekList.last().add(CalendarDate(paddingPrev, DayType.PADDING))
         }
@@ -226,7 +233,6 @@ class YearCalendarView
 
         // 뒤쪽 패딩
         while (paddingNext.dayOfWeek != DayOfWeek.SATURDAY) {
-            // FIXME: 없는 날짜로 수정해야 스케줄 탐색 가능
             paddingNext = paddingNext.plusDays(1)
             weekList.last().add(CalendarDate(paddingNext, DayType.PADDING))
         }
