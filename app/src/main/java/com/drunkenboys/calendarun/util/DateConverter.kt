40aba91abce4d74ff0e3db7820d992a57887ce59
simeunseok <ses8323@gmail.com>
2021-11-09 11:23:24 +0900

    Issue #70 feat: Date Converter 추가

---
+++ app/src/main/java/com/drunkenboys/calendarun/util/DateConverter.kt
@@ -0,0 +1,18 @@
+package com.drunkenboys.calendarun.util
+
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+@SuppressLint("SimpleDateFormat")
+fun dateToString(date: Date): String {
+    val dateFormat = SimpleDateFormat("yyyy.MM.dd")
+
+    return dateFormat.format(date)
+}
+
+@SuppressLint("SimpleDateFormat")
+fun stringToDate(date: String): Date {
+    val df = SimpleDateFormat("yyyy.MM.dd")
+    return df.parse(date)
+}
