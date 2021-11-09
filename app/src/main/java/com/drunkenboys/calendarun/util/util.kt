815bd50b0ea37a19ebf2db90f5857cb56f503112
simeunseok <ses8323@gmail.com>
2021-11-09 11:50:14 +0900

    Revert "Hotfix refactor: util 네이밍 수정"
    This reverts commit 8d076ea6

--- app/src/main/java/com/drunkenboys/calendarun/util/util.kt
+++ app/src/main/java/com/drunkenboys/calendarun/util/util.kt
@@ -1,8 +1,17 @@
-package com.drunkenboys.calendarun.util
+package com.drunkenboys.calendarun
 
+import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.content.Context
+import java.text.SimpleDateFormat
 import java.util.*
+
+@SuppressLint("SimpleDateFormat")
+fun toStringDateFormat(date: Date): String {
+    val dateFormat = SimpleDateFormat("yyyy.MM.dd")
+
+    return dateFormat.format(date)
+}
 
 fun showDatePickerDialog(context: Context, dateSetListener: DatePickerDialog.OnDateSetListener) {
     val cal = Calendar.getInstance()
