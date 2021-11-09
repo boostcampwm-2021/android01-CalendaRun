7975de912ad7c064ba7f44daae950e3d6ea05ee8
cha-ji <chajiwon100785@gmail.com>
2021-11-09 18:35:10 +0900

    Issue #76 feat: 스케줄 나타내기 성공, 정렬 실패

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarView.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarView.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -26,10 +27,12 @@ import com.drunkenboys.ckscalendar.R
 import com.drunkenboys.ckscalendar.databinding.LayoutYearCalendarBinding
 import com.drunkenboys.ckscalendar.FakeFactory
 import com.drunkenboys.ckscalendar.data.CalendarDate
+import com.drunkenboys.ckscalendar.data.CalendarScheduleObject
 import com.drunkenboys.ckscalendar.data.CalendarSet
 import com.drunkenboys.ckscalendar.data.DayType
 import com.drunkenboys.ckscalendar.listener.OnDayClickListener
 import com.drunkenboys.ckscalendar.listener.OnDaySecondClickListener
+import java.time.DayOfWeek
 import java.time.LocalDate
 
 @ExperimentalAnimationApi
@@ -56,6 +59,7 @@ class YearCalendarView
     private var onDateSecondClickListener: OnDaySecondClickListener? = null
 
     init {
+        val scheduleList = FakeFactory.createFakeSchedule()
         val yearList =  mutableListOf<List<CalendarSet>>()
         (INIT_YEAR..LAST_YEAR).forEach { year ->
             yearList.add(FakeFactory.createFakeCalendarSetList(year))
@@ -66,13 +70,13 @@ class YearCalendarView
         }
 
         binding.composeYearCalendarViewYearCalendar.setContent {
-            CalendarLazyColumn(yearList)
+            CalendarLazyColumn(yearList, scheduleList)
         }
     }
 
     @ExperimentalFoundationApi
     @Composable
-    private fun CalendarLazyColumn(yearList: List<List<CalendarSet>>) {
+    private fun CalendarLazyColumn(yearList: List<List<CalendarSet>>, scheduleList: List<CalendarScheduleObject>) {
         // RecyclerView의 상태를 관찰
         val listState = rememberLazyListState()
 
@@ -99,18 +103,22 @@ class YearCalendarView
                                 if (day.dayType == DayType.PADDING) {
                                     PaddingText(day = day)
                                 } else {
-                                    Box(modifier = Modifier
+                                    Column(modifier = Modifier
                                         .layoutId(day.date.toString())
-                                        .border(BorderStroke(width = 2.dp, color = if (clickedDay?.date == day.date) colorResource(id = R.color.primary_color) else Color.White))
+                                        .border(
+                                            BorderStroke(
+                                                width = 2.dp,
+                                                color = if (clickedDay?.date == day.date) colorResource(id = R.color.primary_color)
+                                                else Color.Transparent))
                                         .clickable(onClick = {
                                             clickedDay = day
                                             if (clickedDay?.date != day.date) onDateClickListener
                                             else onDateSecondClickListener
                                         }),
-                                        contentAlignment = Alignment.TopCenter
+                                        horizontalAlignment = Alignment.CenterHorizontally
                                     ) {
                                         DayText(day = day)
-                                        ScheduleText(day = day)
+                                        ScheduleText(day = day ,scheduleList)
                                     }
                                 }
                             }
@@ -139,7 +147,7 @@ class YearCalendarView
                 else -> Color.Black
             },
             modifier = Modifier
-                .padding(bottom = 30.dp),
+                .layoutId(day.date.toString()),
             textAlign = TextAlign.Center,
         )
     }
@@ -156,9 +164,37 @@ class YearCalendarView
     }
 
     @Composable
-    private fun ScheduleText(day: CalendarDate) {
-        // TODO: schedule 탐색
+    private fun ScheduleText(day: CalendarDate, scheduleList: List<CalendarScheduleObject>) {
 
+        // TODO: 보여지는 일정의 정렬
+        val thumbnailList = scheduleList.filter { schedule ->
+            day.date in schedule.startDate..schedule.endDate
+        }.toMutableList()
+
+        while (thumbnailList.size < 3) {
+            thumbnailList.add(CalendarScheduleObject(
+                -1, Color.Transparent.value.toInt(), "", day.date, day.date
+            ))
+        }
+
+        while (thumbnailList.size > 3) {
+            thumbnailList.removeLast()
+        }
+
+        thumbnailList.forEachIndexed { index, schedule ->
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .background(color = Color(schedule.color))) {
+                Text(
+                    text = if (schedule.startDate == day.date || day.date.dayOfWeek == DayOfWeek.SUNDAY) schedule.text else "",
+                    modifier = Modifier.padding(2.dp),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis,
+                    // TODO: font size
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+            }
+        }
     }
 
     private fun getTodayItemIndex(): Int {
