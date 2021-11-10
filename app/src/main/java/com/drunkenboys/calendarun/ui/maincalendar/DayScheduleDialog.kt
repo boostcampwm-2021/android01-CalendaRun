bd8910d683992ffb7192c293bab7c1370fcb4324
Seonghwan Kim <shshksh58@gmail.com>
2021-11-10 12:06:49 +0900

    Issue #90 feat: 다이얼로그 바인딩 적용

--- app/src/main/java/com/drunkenboys/calendarun/ui/maincalendar/DayScheduleDialog.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/maincalendar/DayScheduleDialog.kt
@@ -3,13 +3,35 @@ package com.drunkenboys.calendarun.ui.maincalendar
 import android.app.AlertDialog
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
-import com.drunkenboys.calendarun.R
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.drunkenboys.calendarun.databinding.DialogDayScheduleBinding
+import com.drunkenboys.calendarun.ui.searchschedule.SearchScheduleAdapter
+import com.drunkenboys.calendarun.ui.searchschedule.SearchScheduleViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DayScheduleDialog : DialogFragment() {
 
-    override fun onCreateDialog(savedInstanceState: Bundle?): AlertDialog = AlertDialog.Builder(requireContext())
-        .setView(R.layout.dialog_day_schedule)
-        .create()
+    private var _binding: DialogDayScheduleBinding? = null
+    private val binding get() = _binding ?: throw IllegalStateException()
+
+    private val dayScheduleViewModel by viewModels<SearchScheduleViewModel>()
+
+    private val dayScheduleAdapter = SearchScheduleAdapter()
+
+    private val navController by lazy { findNavController() }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): AlertDialog {
+        _binding = DialogDayScheduleBinding.inflate(layoutInflater)
+
+        return AlertDialog.Builder(requireContext())
+            .setView(binding.root)
+            .create()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
