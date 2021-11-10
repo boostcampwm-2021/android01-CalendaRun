9b8f2953c2c9f6d6efe781a4563797eb70078294
PsPLoG <gkgkgkrj@gmail.com>
2021-11-10 22:07:00 +0900

    Issue #99 feat: CalendarDesignObject.kt Resource Annotation 추가, 기본값과 다크모드 초기 값 지정

--- library/src/main/java/com/drunkenboys/ckscalendar/data/CalendarDesignObject.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/data/CalendarDesignObject.kt
@@ -1,17 +1,36 @@
 package com.drunkenboys.ckscalendar.data
 
-// CalendarView의 디자인을 위한 설정
+import android.graphics.Color
+import android.view.Gravity
+import androidx.annotation.ColorInt
+import androidx.annotation.DrawableRes
+import com.drunkenboys.ckscalendar.R
+
+/**
+ * CalendarView의 디자인을 위한 설정
+ */
 data class CalendarDesignObject(
-    val weekDayTextColor: Int,
-    val holidayTextColor: Int,
-    val saturdayTextColor: Int,
-    val sundayTextColor: Int,
-    val textSize: Int,
-    val textAlign: Int, // FIXME: -1, 0, 1
-    val selectedFrameColor: Int,
-    val backgroundColor: Int,
-    val weekSimpleStringSet: List<String>,
-    val weekFullStringSet: List<String>,
-    val visibleScheduleCount: Int // 조절이 가능할까...?
-    // TODO: frame shape
-)
+    @ColorInt var weekDayTextColor: Int = Color.BLACK,
+    @ColorInt var holidayTextColor: Int = ScheduleColorType.RED.color,
+    @ColorInt var saturdayTextColor: Int = ScheduleColorType.BLUE.color,
+    @ColorInt var sundayTextColor: Int = ScheduleColorType.RED.color,
+    @ColorInt var selectedFrameColor: Int = ScheduleColorType.GRAY.color,
+    @ColorInt var backgroundColor: Int = Color.WHITE,
+    @DrawableRes var selectedFrameDrawable: Int = R.drawable.bg_month_date_selected,
+    var textSize: Int = 10,
+    var textAlign: Int = Gravity.CENTER_VERTICAL,
+    var weekSimpleStringSet: List<String> = listOf(),
+    var weekFullStringSet: List<String> = listOf(),
+    var visibleScheduleCount: Int = 3,
+) {
+    companion object {
+
+        fun getDefaultDesign() = CalendarDesignObject()
+
+        fun getDarkDesign() = getDefaultDesign().apply {
+            backgroundColor = Color.BLACK
+            weekDayTextColor = Color.WHITE
+            selectedFrameColor = Color.WHITE
+        }
+    }
+}
