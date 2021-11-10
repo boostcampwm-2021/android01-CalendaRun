d712f1fc417bc3a09654718588c3e49fb727d71a
PsPLoG <gkgkgkrj@gmail.com>
2021-11-10 22:07:33 +0900

    Issue #99 feat: drawable stroke 색상과 두께를 변경할 수 있는 Extension 추가

--- library/src/main/java/com/drunkenboys/ckscalendar/utils/ViewExtensions.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/utils/ViewExtensions.kt
@@ -1,10 +1,36 @@
 package com.drunkenboys.ckscalendar.utils
 
 import android.content.Context
+import android.graphics.drawable.DrawableContainer
+import android.graphics.drawable.GradientDrawable
+import android.graphics.drawable.StateListDrawable
 import android.util.TypedValue
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
+
 fun Context.dp2px(dp: Float) = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, resources.displayMetrics)
+
+fun View.tintStroke(color: Int, width: Float) {
+    this.resources.displayMetrics.density.let { scale ->
+        this.background?.constantState?.let {
+            val origin = (it.newDrawable().mutate())
+            when (origin) {
+                is GradientDrawable -> origin.setStroke((width * scale).toInt(), color)
+                is StateListDrawable -> {
+                    val drawableContainerState = origin.constantState as DrawableContainer.DrawableContainerState
+                    val children = drawableContainerState.children
+                    children.filterNotNull()
+                        .forEach {
+                            it as GradientDrawable
+                            it.setStroke((width * scale).toInt(), color)
+                        }
+                }
+            }
+            this.background = origin
+        }
+    }
+}
 
 fun RecyclerView.ViewHolder.context(): Context {
     return this.itemView.context
