d48a5b7b797b4434cbd4b015d6ab983f4729e8dc
Seonghwan Kim <shshksh58@gmail.com>
2021-11-10 16:59:25 +0900

    Issue #90 feat: Dialog -> SaveSchedule 내비게이션 설정

--- app/src/main/java/com/drunkenboys/calendarun/ui/dayschedule/DayScheduleDialog.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/dayschedule/DayScheduleDialog.kt
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.drunkenboys.calendarun.R
 import com.drunkenboys.calendarun.databinding.DialogDayScheduleBinding
+import com.drunkenboys.calendarun.ui.saveschedule.model.BehaviorType
 import com.drunkenboys.calendarun.ui.searchschedule.SearchScheduleAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import java.time.LocalDate
@@ -30,11 +31,37 @@ class DayScheduleDialog : DialogFragment() {
         dayScheduleViewModel.fetchScheduleList(LocalDate.parse(args.localDate))
 
         initRvDaySchedule()
+        initIvAddSchedule()
         observeListItem()
+        observeScheduleClickEvent()
 
         return AlertDialog.Builder(requireContext())
             .setView(binding.root)
             .create()
+    }
+
+    private fun initRvDaySchedule() {
+        binding.rvDaySchedule.adapter = dayScheduleAdapter
+    }
+
+    private fun initIvAddSchedule() {
+        binding.ivDayScheduleAddSchedule.setOnClickListener {
+            val action = DayScheduleDialogDirections.actionDayScheduleDialogToSaveScheduleFragment(BehaviorType.INSERT)
+            navController.navigate(action)
+        }
+    }
+
+    private fun observeListItem() {
+        dayScheduleViewModel.listItem.observe(this) { listItem ->
+            dayScheduleAdapter.submitList(listItem)
+        }
+    }
+
+    private fun observeScheduleClickEvent() {
+        dayScheduleViewModel.scheduleClickEvent.observe(this) {
+            val action = DayScheduleDialogDirections.actionDayScheduleDialogToSaveScheduleFragment(BehaviorType.UPDATE)
+            navController.navigate(action)
+        }
     }
 
     override fun onResume() {
@@ -43,16 +70,6 @@ class DayScheduleDialog : DialogFragment() {
         val displayMetrics = resources.displayMetrics
         dialog?.window?.setBackgroundDrawableResource(R.drawable.bg_white_radius_24dp)
         dialog?.window?.setLayout((displayMetrics.widthPixels * 0.9).toInt(), (displayMetrics.heightPixels * 0.7).toInt())
-    }
-
-    private fun initRvDaySchedule() {
-        binding.rvDaySchedule.adapter = dayScheduleAdapter
-    }
-
-    private fun observeListItem() {
-        dayScheduleViewModel.listItem.observe(this) { listItem ->
-            dayScheduleAdapter.submitList(listItem)
-        }
     }
 
     override fun onDestroyView() {
