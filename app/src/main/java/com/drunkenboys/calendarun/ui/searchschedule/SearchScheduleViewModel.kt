eec604feee32af470925b8af0168d4e4bc1cd8cd
Seonghwan Kim <shshksh58@gmail.com>
2021-11-10 15:59:34 +0900

    Issue #90 refactor: DateItem 프로퍼티 타입 변경
    Date -> LocalDate

--- app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleViewModel.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/SearchScheduleViewModel.kt
@@ -11,13 +11,11 @@ import com.drunkenboys.calendarun.ui.searchschedule.model.DateItem
 import com.drunkenboys.calendarun.ui.searchschedule.model.DateScheduleItem
 import com.drunkenboys.calendarun.util.SingleLiveEvent
 import com.drunkenboys.calendarun.util.extensions.getOrThrow
-import com.drunkenboys.calendarun.util.localDateTimeToDate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
-import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -56,12 +54,12 @@ class SearchScheduleViewModel @Inject constructor(
         }
     }
 
-    private fun List<Schedule>.mapToDateItem() = groupBy { schedule -> schedule.dayMillis() }
-        .map { (dayMillis, scheduleList) ->
+    private fun List<Schedule>.mapToDateItem() = groupBy { schedule -> schedule.startDate.toLocalDate() }
+        .map { (localDate, scheduleList) ->
             val dateScheduleList = scheduleList.map { schedule ->
                 DateScheduleItem(schedule) { emitScheduleClickEvent(schedule) }
             }
-            DateItem(Date(dayMillis), dateScheduleList)
+            DateItem(localDate, dateScheduleList)
         }
         .sortedBy { dateItem -> dateItem.date }
 
@@ -70,14 +68,6 @@ class SearchScheduleViewModel @Inject constructor(
         IdStore.putId(IdStore.KEY_SCHEDULE_ID, schedule.id)
         _scheduleClickEvent.value = Unit
     }
-
-    private fun Schedule.dayMillis() = Calendar.getInstance().apply {
-        time = localDateTimeToDate(this@dayMillis.startDate)
-        set(Calendar.HOUR_OF_DAY, 0)
-        set(Calendar.MINUTE, 0)
-        set(Calendar.SECOND, 0)
-        set(Calendar.MILLISECOND, 0)
-    }.timeInMillis
 
     fun searchSchedule(word: String) {
         debounceJob.cancel()
