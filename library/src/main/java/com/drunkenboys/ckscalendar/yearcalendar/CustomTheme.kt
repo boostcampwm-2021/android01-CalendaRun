938be9042b27c609fb4c3a0842073340184ec900
cha-ji <chajiwon100785@gmail.com>
2021-11-11 16:43:10 +0900

    Issue #127 feat: 머티리얼 테마에서 커스텀한 Theme 파일 생성

---
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/CustomTheme.kt
@@ -0,0 +1,37 @@
+package com.drunkenboys.ckscalendar.yearcalendar
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import com.drunkenboys.ckscalendar.data.CalendarDesignObject
+
+@Composable
+fun CustomTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    design: CalendarDesignObject,
+    content: @Composable () -> Unit
+) {
+    val darkColors = darkColors(
+        primary = Color(design.weekDayTextColor),
+        primaryVariant = Color(design.selectedFrameColor),
+        secondary = Color(design.saturdayTextColor),
+        secondaryVariant = Color(design.sundayTextColor),
+        background = Color(design.backgroundColor),
+    )
+
+    val lightColors = lightColors(
+        primary = Color(design.weekDayTextColor),
+        primaryVariant = Color(design.selectedFrameColor),
+        secondary = Color(design.saturdayTextColor),
+        secondaryVariant = Color(design.sundayTextColor),
+        background = Color(design.backgroundColor),
+    )
+
+    MaterialTheme(
+        colors = if (darkTheme) darkColors else lightColors,
+        content = content
+    )
+}
