6ad63f1bf826d10349a3d1cde904a1c6f367f1c7
simeunseok <ses8323@gmail.com>
2021-11-11 12:29:09 +0900

    Issue #100 refactor: DateConverter 포맷 수정

--- app/src/main/java/com/drunkenboys/calendarun/util/DateConverter.kt
+++ app/src/main/java/com/drunkenboys/calendarun/util/DateConverter.kt
@@ -8,19 +8,9 @@ import java.time.format.DateTimeFormatter
 
 val defaultZoneOffset = ZoneOffset.systemDefault().rules.getOffset(Instant.now())
 
-fun localDateToString(date: LocalDate): String = date.format(DateTimeFormatter.ofPattern("yyyy.MM.dd"));
+fun localDateToString(date: LocalDate): String = date.format(DateTimeFormatter.ofPattern("yyyy.M.d"));
 
-fun stringToLocalDate(date: String): LocalDate {
-    // TODO
-    var pattern = ""
-    val dateList = date.split(".")
-    repeat(dateList[0].length) { pattern += "y" }
-    pattern += "."
-    repeat(dateList[1].length) { pattern += "M" }
-    pattern += "."
-    repeat(dateList[2].length) { pattern += "d" }
-
-    return LocalDate.parse(date, DateTimeFormatter.ofPattern(pattern))
-}
+fun stringToLocalDate(date: String): LocalDate =
+    LocalDate.parse(date, DateTimeFormatter.ofPattern("yyyy.M.d"))
 
 fun Long.toDefaultLocalDateTime(): LocalDateTime = LocalDateTime.ofEpochSecond(this / 1000, 0, defaultZoneOffset)
