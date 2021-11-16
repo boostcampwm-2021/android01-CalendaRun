601e172ce22f94d39000b8bb18c759fc9cb3f437
Seonghwan Kim <shshksh58@gmail.com>
2021-11-16 17:25:19 +0900

    Issue #150 layout: ThemeFragment 레이아웃 작성

---
+++ app/src/main/java/com/drunkenboys/calendarun/ui/theme/ThemeFragment.kt
@@ -0,0 +1,8 @@
+package com.drunkenboys.calendarun.ui.theme
+
+import com.drunkenboys.calendarun.R
+import com.drunkenboys.calendarun.databinding.FragmentThemeBinding
+import com.drunkenboys.calendarun.ui.base.BaseFragment
+
+class ThemeFragment : BaseFragment<FragmentThemeBinding>(R.layout.fragment_theme) {
+}
