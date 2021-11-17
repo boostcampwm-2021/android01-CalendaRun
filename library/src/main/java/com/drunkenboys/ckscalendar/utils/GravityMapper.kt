e8934225624f5b2e06e71bea913e46c15af01078
cha-ji <chajiwon100785@gmail.com>
2021-11-17 22:40:01 +0900

    Issue #94 feat: gravity -> Alignment.Horizontal mapper 클래스 생성

--- library/src/main/java/com/drunkenboys/ckscalendar/utils/GravityMapper.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/utils/GravityMapper.kt
@@ -1,6 +1,8 @@
 package com.drunkenboys.ckscalendar.utils
 
+import android.text.Layout
 import android.view.Gravity
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.text.style.TextAlign
 
 object GravityMapper {
@@ -11,5 +13,12 @@ object GravityMapper {
         Gravity.LEFT -> TextAlign.Left
         Gravity.START -> TextAlign.Start
         else -> TextAlign.Center
+    }
+
+    fun toColumnAlign(gravity: Int): Alignment.Horizontal = when (gravity) {
+        Gravity.CENTER -> Alignment.CenterHorizontally
+        Gravity.END -> Alignment.End
+        Gravity.START -> Alignment.Start
+        else -> Alignment.CenterHorizontally
     }
 }
