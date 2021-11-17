dd0c326defcef089cb2b7b4f0179003a4c2516b3
simeunseok <ses8323@gmail.com>
2021-11-17 15:46:05 +0900

    Issue #169 refactor: 속성 추가
    CheckPointItem에 id 속성값 추가

--- app/src/main/java/com/drunkenboys/calendarun/ui/savecalendar/model/CheckPointItem.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/savecalendar/model/CheckPointItem.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import java.time.LocalDate
 
 data class CheckPointItem(
+    val id: Long = 0L,
     val name: MutableStateFlow<String> = MutableStateFlow(""),
     val date: MutableStateFlow<LocalDate?> = MutableStateFlow(null),
     var check: Boolean = false
