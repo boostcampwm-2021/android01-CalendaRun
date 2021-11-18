445942cff36247ce2ed08a2f892f20a85e10ad45
Seonghwan Kim <shshksh58@gmail.com>
2021-11-18 17:54:49 +0900

    Issue #150 feat: ThemeResetDialog 내비게이션 설정

--- app/src/main/java/com/drunkenboys/calendarun/ui/theme/ThemeFragment.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/theme/ThemeFragment.kt
@@ -3,7 +3,8 @@ package com.drunkenboys.calendarun.ui.theme
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
-import androidx.fragment.app.viewModels
+import androidx.navigation.navGraphViewModels
+import androidx.navigation.ui.setupWithNavController
 import com.drunkenboys.calendarun.R
 import com.drunkenboys.calendarun.databinding.FragmentThemeBinding
 import com.drunkenboys.calendarun.ui.base.BaseFragment
@@ -13,13 +14,31 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class ThemeFragment : BaseFragment<FragmentThemeBinding>(R.layout.fragment_theme) {
 
-    private val themeViewModel by viewModels<ThemeViewModel>()
+    private val themeViewModel by navGraphViewModels<ThemeViewModel>(R.id.themeFragment) { defaultViewModelProviderFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.viewModel = themeViewModel
 
+        setupToolbar()
         collectTextAlign()
+    }
+
+    private fun setupToolbar() {
+        binding.toolbarTheme.setupWithNavController(navController)
+        binding.toolbarTheme.setOnMenuItemClickListener { menu ->
+            if (menu.itemId == R.id.menu_theme_reset) {
+                navigateToThemeResetDialog()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun navigateToThemeResetDialog() {
+        val action = ThemeFragmentDirections.toThemeResetDialog()
+        navController.navigate(action)
     }
 
     private fun collectTextAlign() {
