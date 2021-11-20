64e88ca3c0ab05bc2785aa5c24730ae62040dff4
simeunseok <ses8323@gmail.com>
2021-11-20 23:22:07 +0900

    Issue #197 refactor: Calendar column 변경

--- app/src/main/java/com/drunkenboys/calendarun/data/calendar/entity/Calendar.kt
+++ app/src/main/java/com/drunkenboys/calendarun/data/calendar/entity/Calendar.kt
@@ -2,12 +2,9 @@ package com.drunkenboys.calendarun.data.calendar.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.time.LocalDate
 
 @Entity(tableName = "Calendar")
 data class Calendar(
     @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val name: String,
-    val startDate: LocalDate,
-    val endDate: LocalDate
+    val name: String
 )
