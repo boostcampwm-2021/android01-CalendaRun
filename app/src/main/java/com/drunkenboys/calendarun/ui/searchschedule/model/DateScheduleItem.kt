e95c66f6ee75987a25c401a9e8144b610d98761a
Seonghwan Kim <shshksh58@gmail.com>
2021-11-22 10:37:35 +0900

    Issue #199 feat: 오전, 오후 표기 추가

--- app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/model/DateScheduleItem.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/model/DateScheduleItem.kt
@@ -2,13 +2,14 @@ package com.drunkenboys.calendarun.ui.searchschedule.model
 
 import androidx.recyclerview.widget.DiffUtil
 import com.drunkenboys.calendarun.data.schedule.entity.Schedule
+import com.drunkenboys.calendarun.util.amPm
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 data class DateScheduleItem(val schedule: Schedule, val onClick: () -> Unit) {
 
     val duration: String = run {
-        val startDateFormat = DateTimeFormatter.ofPattern("hh:mm")
+        val startDateFormat = DateTimeFormatter.ofPattern("${schedule.startDate.amPm} hh:mm")
         val endDateFormat = getEndDateFormat(schedule.startDate, schedule.endDate)
 
         "${schedule.startDate.format(startDateFormat)} ~ ${schedule.endDate.format(endDateFormat)}"
@@ -16,10 +17,10 @@ data class DateScheduleItem(val schedule: Schedule, val onClick: () -> Unit) {
 
     private fun getEndDateFormat(startDate: LocalDateTime, endDate: LocalDateTime): DateTimeFormatter {
         return when {
-            startDate.year < endDate.year -> DateTimeFormatter.ofPattern("yyyy년 M월 d일 hh:mm")
-            startDate.month < endDate.month -> DateTimeFormatter.ofPattern("M월 d일 hh:mm")
-            startDate.dayOfYear < endDate.dayOfYear -> DateTimeFormatter.ofPattern("d일 hh:mm")
-            else -> DateTimeFormatter.ofPattern("hh:mm")
+            startDate.year < endDate.year -> DateTimeFormatter.ofPattern("yyyy년 M월 d일 ${endDate.amPm} hh:mm")
+            startDate.month < endDate.month -> DateTimeFormatter.ofPattern("M월 d일 ${endDate.amPm} hh:mm")
+            startDate.dayOfYear < endDate.dayOfYear -> DateTimeFormatter.ofPattern("d일 ${endDate.amPm} hh:mm")
+            else -> DateTimeFormatter.ofPattern("${endDate.amPm} hh:mm")
         }
     }
 
