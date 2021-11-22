2fa4ed98871891ca6f2e9ede0bb59f80af152c2a
차지원 <chajiwon100785@gmail.com>
2021-11-22 18:24:08 +0900

    Issue #211 feat: month header에 그림자로 입체감 추가

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/MonthHeader.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/MonthHeader.kt
@@ -3,13 +3,8 @@ package com.drunkenboys.ckscalendar.yearcalendar.composeView
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.scrollBy
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
@@ -17,12 +12,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.drunkenboys.ckscalendar.R
 import com.drunkenboys.ckscalendar.yearcalendar.CustomTheme
 import com.drunkenboys.ckscalendar.yearcalendar.YearCalendarViewModel
 
@@ -40,19 +31,19 @@ fun AnimatedMonthHeader(
     )
 
     Card(
-        modifier = Modifier
-            .alpha(density)
-            .padding(start = (density * 5).dp)
+        modifier = Modifier.alpha(density),
+        elevation = 10.dp
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.ic_main_background),
-            contentDescription = "월 아이콘", // FIXME strings.xml
-            modifier = Modifier.clip(shape = RoundedCornerShape(16.dp))
-        )
-
         Text(
             text = monthName,
             color = MaterialTheme.colors.primary,
+            modifier = Modifier
+                .padding(
+                    start = 20.dp,
+                    end = 20.dp,
+                    top = 5.dp,
+                    bottom = 5.dp
+                )
         )
     }
 }
@@ -62,17 +53,20 @@ fun AnimatedMonthHeader(
 fun PreviewMonthHeader() {
     val viewModel = YearCalendarViewModel()
     CustomTheme(design = viewModel.design.value) {
-        Card {
-            Image(
-                painter = painterResource(id = R.drawable.ic_main_background),
-                contentDescription = "월 아이콘", // FIXME strings.xml
-                modifier = Modifier.clip(shape = RoundedCornerShape(16.dp))
-            )
-
+        Card(
+            modifier = Modifier.wrapContentSize(),
+            elevation = 200.dp
+        ) {
             Text(
                 text = "1월",
                 color = MaterialTheme.colors.primary,
-                modifier = Modifier.padding(start = 5.dp)
+                modifier = Modifier
+                    .padding(
+                        start = 20.dp,
+                        end = 20.dp,
+                        top = 5.dp,
+                        bottom = 5.dp
+                    )
             )
         }
     }
