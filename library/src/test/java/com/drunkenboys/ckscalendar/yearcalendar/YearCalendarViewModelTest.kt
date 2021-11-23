0888d9d9890d89690dc36e6c926fd90492d506d7
차지원 <chajiwon100785@gmail.com>
2021-11-23 10:36:13 +0900

    Issue #234 test: Before 셋업 테스트함수 추가

--- library/src/test/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarViewModelTest.kt
+++ library/src/test/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarViewModelTest.kt
@@ -2,22 +2,29 @@ package com.drunkenboys.ckscalendar.yearcalendar
 
 import com.drunkenboys.ckscalendar.data.CalendarScheduleObject
 import org.junit.Assert.*
+import org.junit.Before
 
 import org.junit.Test
 import java.time.LocalDateTime
 
 class YearCalendarViewModelTest {
 
-    private val viewModel = YearCalendarViewModel()
+    private lateinit var viewModel: YearCalendarViewModel
     private val today = LocalDateTime.now()
-    private val fakeSchedules = (1..10).map { i ->
-        CalendarScheduleObject(
-            id = i,
-            color = viewModel.design.value.weekDayTextColor,
-            text = "$i",
-            startDate = today.minusDays(i.toLong()),
-            endDate = today.plusDays(i.toLong())
-        )
+    private lateinit var fakeSchedules: List<CalendarScheduleObject>
+
+    @Before
+    fun setUp() {
+        viewModel = YearCalendarViewModel()
+        fakeSchedules = (1..10).map { i ->
+            CalendarScheduleObject(
+                id = i,
+                color = viewModel.design.value.weekDayTextColor,
+                text = "$i",
+                startDate = today.minusDays(i.toLong()),
+                endDate = today.plusDays(i.toLong())
+            )
+        }
     }
 
     @Test
