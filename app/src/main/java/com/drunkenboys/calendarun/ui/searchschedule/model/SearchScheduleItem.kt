c148cedb335d1b72631c9fcc54b70885e305d2f6
Seonghwan Kim <shshksh58@gmail.com>
2021-11-23 20:50:32 +0900

    Issue #248 refactor: SearchSchedule 리사이클러 뷰 데이터 타입 변경
    페이징을 위해 기존의 방식에서 멀티 뷰 타입으로 변경함.

--- app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/model/SearchScheduleItem.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/searchschedule/model/SearchScheduleItem.kt
@@ -3,16 +3,36 @@ package com.drunkenboys.calendarun.ui.searchschedule.model
 import androidx.recyclerview.widget.DiffUtil
 import com.drunkenboys.calendarun.data.schedule.entity.Schedule
 import com.drunkenboys.calendarun.util.amPm
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-data class DateScheduleItem(val schedule: Schedule, val onClick: () -> Unit) {
+sealed class SearchScheduleItem
 
-    val duration: String = run {
+data class DateItem(val date: LocalDate) : SearchScheduleItem() {
+
+    override fun toString(): String = date.format(DateTimeFormatter.ofPattern("M월 d일"))
+
+    companion object {
+
+        val diffUtil by lazy {
+            object : DiffUtil.ItemCallback<DateItem>() {
+
+                override fun areItemsTheSame(oldItem: DateItem, newItem: DateItem) = oldItem.hashCode() == newItem.hashCode()
+
+                override fun areContentsTheSame(oldItem: DateItem, newItem: DateItem) = oldItem == newItem
+            }
+        }
+    }
+}
+
+data class DateScheduleItem(val schedule: Schedule, val onClick: () -> Unit) : SearchScheduleItem() {
+
+    override fun toString(): String {
         val startDateFormat = DateTimeFormatter.ofPattern("${schedule.startDate.amPm} hh:mm")
         val endDateFormat = getEndDateFormat(schedule.startDate, schedule.endDate)
 
-        "${schedule.startDate.format(startDateFormat)} ~ ${schedule.endDate.format(endDateFormat)}"
+        return "${schedule.startDate.format(startDateFormat)} ~ ${schedule.endDate.format(endDateFormat)}"
     }
 
     private fun getEndDateFormat(startDate: LocalDateTime, endDate: LocalDateTime): DateTimeFormatter {
