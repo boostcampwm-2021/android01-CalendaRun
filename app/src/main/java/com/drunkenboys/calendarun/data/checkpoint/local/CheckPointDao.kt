8a650c90442ca127fba3f30e53032c9d6a7ac0e1
simeunseok <ses8323@gmail.com>
2021-11-25 01:25:14 +0900

    Issue #261 feat: updateCheckPoint 쿼리 추가

--- app/src/main/java/com/drunkenboys/calendarun/data/checkpoint/local/CheckPointDao.kt
+++ app/src/main/java/com/drunkenboys/calendarun/data/checkpoint/local/CheckPointDao.kt
@@ -3,6 +3,7 @@ package com.drunkenboys.calendarun.data.checkpoint.local
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
+import androidx.room.Update
 import com.drunkenboys.calendarun.data.checkpoint.entity.CheckPoint
 
 @Dao
@@ -20,4 +21,7 @@ interface CheckPointDao {
     @Query("SELECT * FROM `checkpoint` WHERE calendarId == :calendarId")
     suspend fun fetchCalendarCheckPoints(calendarId: Long): List<CheckPoint>
 
+    @Update
+    suspend fun updateCheckPoint(checkPoint: CheckPoint)
+    
 }
