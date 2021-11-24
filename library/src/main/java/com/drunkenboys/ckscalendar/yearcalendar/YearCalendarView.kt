9dbe015f106ea621e0930dee7b9bd51712f83ea6
차지원 <chajiwon100785@gmail.com>
2021-11-24 17:44:55 +0900

    Issue #251 feat: attr 속성 추가

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarView.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/YearCalendarView.kt
@@ -1,12 +1,15 @@
 package com.drunkenboys.ckscalendar.yearcalendar
 
 import android.content.Context
+import android.content.res.Configuration
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import androidx.compose.foundation.layout.Column
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingMethod
 import androidx.databinding.BindingMethods
+import com.drunkenboys.ckscalendar.R
 import com.drunkenboys.ckscalendar.databinding.LayoutYearCalendarBinding
 import com.drunkenboys.ckscalendar.data.*
 import com.drunkenboys.ckscalendar.listener.OnDayClickListener
@@ -31,7 +34,7 @@ import java.time.LocalDateTime
 class YearCalendarView
 @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
+    private val attrs: AttributeSet? = null,
     defStyleAttr: Int = 0,
 ) : LinearLayout(context, attrs, defStyleAttr) {
 
@@ -43,6 +46,8 @@ class YearCalendarView
     private var viewModel = YearCalendarViewModel()
 
     init {
+        setAttr()
+
         binding.composeYearCalendarViewYearCalendar.setContent {
             CustomTheme(design = viewModel.design.value) {
                 Column {
@@ -54,6 +59,38 @@ class YearCalendarView
                     )
                 }
             }
+        }
+    }
+
+    private fun setAttr() {
+        val attr = context.obtainStyledAttributes(attrs, R.styleable.MonthCalendarView)
+        val weekDayTextColor =
+            attr.getColor(R.styleable.MonthCalendarView_weekDayTextColor, ContextCompat.getColor(context, R.color.calendar_black))
+        val holidayTextColor = attr.getColor(R.styleable.MonthCalendarView_holidayTextColor, ScheduleColorType.RED.color)
+        val saturdayTextColor = attr.getColor(R.styleable.MonthCalendarView_saturdayTextColor, ScheduleColorType.BLUE.color)
+        val sundayTextColor = attr.getColor(R.styleable.MonthCalendarView_sundayTextColor, ScheduleColorType.RED.color)
+        val selectedFrameColor = attr.getColor(R.styleable.MonthCalendarView_selectedFrameColor, ScheduleColorType.ORANGE.color)
+        val backgroundColor =
+            attr.getColor(R.styleable.MonthCalendarView_backgroundColor, ContextCompat.getColor(context, R.color.calendar_white))
+        val selectedFrameDrawable =
+            attr.getResourceId(R.styleable.MonthCalendarView_selectedFrameDrawable, R.drawable.bg_month_date_selected)
+
+        attr.recycle()
+
+        val design = when (context.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK)) {
+            Configuration.UI_MODE_NIGHT_YES -> CalendarDesignObject.getDarkDesign()
+            else -> CalendarDesignObject.getDefaultDesign()
+        }
+
+        design.apply {
+            this.weekDayTextColor = weekDayTextColor
+            this.holidayTextColor = holidayTextColor
+            this.saturdayTextColor = saturdayTextColor
+            this.sundayTextColor = sundayTextColor
+            this.selectedFrameColor = selectedFrameColor
+            this.backgroundColor = backgroundColor
+            this.selectedFrameDrawable = selectedFrameDrawable
+            viewModel.setDesign(this)
         }
     }
 
