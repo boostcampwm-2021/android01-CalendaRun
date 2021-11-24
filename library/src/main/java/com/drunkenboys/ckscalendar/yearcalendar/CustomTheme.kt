6751194da9e06bc45f9bf458d7556474052a7ce0
차지원 <chajiwon100785@gmail.com>
2021-11-24 17:44:44 +0900

    Issue #251 fix: 다크테마 지원 안함

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/CustomTheme.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/CustomTheme.kt
@@ -1,29 +1,16 @@
 package com.drunkenboys.ckscalendar.yearcalendar
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.colorResource
-import com.drunkenboys.ckscalendar.R
 import com.drunkenboys.ckscalendar.data.CalendarDesignObject
 
 @Composable
 fun CustomTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     design: CalendarDesignObject,
     content: @Composable () -> Unit
 ) {
-    val darkColors = darkColors(
-        primary = colorResource(id = R.color.calendar_white),
-        primaryVariant = Color(design.selectedFrameColor),
-        secondary = Color(design.saturdayTextColor),
-        secondaryVariant = Color(design.sundayTextColor),
-        background = colorResource(id = R.color.calendar_black),
-    )
-
     val lightColors = lightColors(
         primary = Color(design.weekDayTextColor),
         primaryVariant = Color(design.selectedFrameColor),
@@ -33,7 +20,7 @@ fun CustomTheme(
     )
 
     MaterialTheme(
-        colors = if (darkTheme && design == CalendarDesignObject.getDefaultDesign()) darkColors else lightColors,
+        colors = lightColors,
         content = content
     )
 }
