7362406fee0f14b653b2f3c9e37b030c8c9ecb64
simeunseok <ses8323@gmail.com>
2021-11-25 00:04:54 +0900

    Issue #261 refactor: Insert onConflict 옵션 삭제

--- app/src/main/java/com/drunkenboys/calendarun/data/calendar/local/CalendarDao.kt
+++ app/src/main/java/com/drunkenboys/calendarun/data/calendar/local/CalendarDao.kt
@@ -3,14 +3,13 @@ package com.drunkenboys.calendarun.data.calendar.local
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.REPLACE
 import androidx.room.Query
 import com.drunkenboys.calendarun.data.calendar.entity.Calendar
 
 @Dao
 interface CalendarDao {
 
-    @Insert(onConflict = REPLACE)
+    @Insert
     suspend fun insertCalendar(calendar: Calendar): Long
 
     @Query("SELECT * FROM `calendar`")
