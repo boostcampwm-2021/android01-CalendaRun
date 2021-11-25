27db50dcac9f646473a38b8995aa5618c6c48cba
Seonghwan Kim <shshksh58@gmail.com>
2021-11-25 20:05:23 +0900

    Issue #258 feat: 일정 다이얼로그의 시간 형식 변경
    다이얼로그 시간에 상대적인 시간 형식으로 보이도록 수정.

--- app/src/main/java/com/drunkenboys/calendarun/ui/dayschedule/DayScheduleAdapter.kt
+++ app/src/main/java/com/drunkenboys/calendarun/ui/dayschedule/DayScheduleAdapter.kt
@@ -6,14 +6,19 @@ import com.drunkenboys.calendarun.R
 import com.drunkenboys.calendarun.databinding.ItemScheduleBinding
 import com.drunkenboys.calendarun.ui.base.BaseViewHolder
 import com.drunkenboys.calendarun.ui.searchschedule.model.ScheduleItem
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
-class DayScheduleAdapter : ListAdapter<ScheduleItem, BaseViewHolder<ItemScheduleBinding>>(ScheduleItem.diffUtil) {
+class DayScheduleAdapter(private val localDate: LocalDate) :
+    ListAdapter<ScheduleItem, BaseViewHolder<ItemScheduleBinding>>(ScheduleItem.diffUtil) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ItemScheduleBinding> =
         BaseViewHolder(parent, R.layout.item_schedule)
 
     override fun onBindViewHolder(holder: BaseViewHolder<ItemScheduleBinding>, position: Int) {
         holder.binding.item = currentList[position]
+        holder.binding.time = LocalDateTime.of(localDate, LocalTime.MIN)
         holder.binding.executePendingBindings()
     }
 }
