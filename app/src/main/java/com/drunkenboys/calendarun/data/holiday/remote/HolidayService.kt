144ab662e235a270fa3a07097b48119d016d5eba
simeunseok <ses8323@gmail.com>
2021-11-25 23:19:48 +0900

    Issue #281 feat: HolidayService 정의
    년/월로 공휴일 일정 가져오는 함수 구현

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/holiday/remote/HolidayService.kt
@@ -0,0 +1,18 @@
+package com.drunkenboys.calendarun.data.holiday.remote
+
+import com.drunkenboys.calendarun.BuildConfig
+import com.drunkenboys.calendarun.data.holiday.ResponseHolidayInfo
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface HolidayService {
+
+    @GET("/B090041/openapi/service/SpcdeInfoService/getRestDeInfo")
+    suspend fun fetchHolidayOnMonth(
+        @Query("solYear") year: Int,
+        @Query("solMonth") month: Int,
+        @Query("_type") type: String = "json",
+        @Query("ServiceKey") serviceKey: String = BuildConfig.HOLIDAY_API_KEY
+    ): ResponseHolidayInfo
+
+}
