44771e38e2fb128242e1bf0ff499ac61e13ba8d6
차지원 <chajiwon100785@gmail.com>
2021-11-29 18:32:42 +0900

    Issue #288 fix: 일정 시작 패딩 추가

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/ScheduleText.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/ScheduleText.kt
@@ -1,10 +1,7 @@
 package com.drunkenboys.ckscalendar.yearcalendar.composeView
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CutCornerShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
@@ -33,28 +30,22 @@ fun ScheduleText(
     viewModel.setWeekSchedules(weekScheduleList, today)
 
     weekScheduleList[today.dayOfWeek.dayValue()].forEach { schedule ->
-        Text(
-            text = if (schedule != null && (schedule.startDate.toLocalDate() == today || today.dayOfWeek == DayOfWeek.SUNDAY))
-                schedule.text else " ",
-            maxLines = 1,
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(
-                    color = if (schedule != null) Color(schedule.color) else Color.Transparent,
-                    shape = when {
-                        schedule != null && (schedule.endDate.toLocalDate() == today) ->
-                            RoundedCornerShape(topEnd = 20.dp, bottomEnd = 20.dp)
-                        schedule != null && (schedule.startDate.toLocalDate() == today) ->
-                            RoundedCornerShape(topStart = 20.dp, bottomStart = 20.dp)
-                        else ->
-                            RectangleShape
-                    }
-                )
-                .padding(4.dp),
-            overflow = TextOverflow.Ellipsis,
-            fontSize = viewModel.design.value.textSize.dp(),
-            color = Color.White
-        )
+        Row {
+            if (schedule != null && (schedule.startDate.toLocalDate() == today)) Spacer(modifier = Modifier.width(10.dp))
+
+            Text(
+                text = if (schedule != null && (schedule.startDate.toLocalDate() == today || today.dayOfWeek == DayOfWeek.SUNDAY))
+                    schedule.text else " ",
+                maxLines = 1,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(color = if (schedule != null) Color(schedule.color) else Color.Transparent)
+                    .padding(4.dp),
+                overflow = TextOverflow.Ellipsis,
+                fontSize = viewModel.design.value.textSize.dp(),
+                color = Color.White
+            )
+        }
         Spacer(modifier = Modifier.height(2.dp))
     }
 }
