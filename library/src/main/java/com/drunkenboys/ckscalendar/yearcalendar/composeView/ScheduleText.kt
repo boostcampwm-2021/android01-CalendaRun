9a259d918dd2f4602a347f7ef2dbbab849717b33
차지원 <chajiwon100785@gmail.com>
2021-11-29 17:09:09 +0900

    Issue #288 feat: 일정의 시작과 끝에 둥근 코너 처리

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/ScheduleText.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/ScheduleText.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.drunkenboys.ckscalendar.data.CalendarScheduleObject
 import com.drunkenboys.ckscalendar.utils.TimeUtils.dayValue
@@ -33,7 +37,17 @@ fun ScheduleText(
             maxLines = 1,
             modifier = Modifier
                 .fillMaxWidth()
-                .background(color = if (schedule != null) Color(schedule.color) else Color.Transparent)
+                .background(
+                    color = if (schedule != null) Color(schedule.color) else Color.Transparent,
+                    shape = when {
+                        schedule != null && (schedule.endDate.toLocalDate() == today) ->
+                            RoundedCornerShape(topEnd = 20.dp, bottomEnd = 20.dp)
+                        schedule != null && (schedule.startDate.toLocalDate() == today) ->
+                            RoundedCornerShape(topStart = 20.dp, bottomStart = 20.dp)
+                        else ->
+                            RectangleShape
+                    }
+                )
                 .padding(4.dp),
             overflow = TextOverflow.Ellipsis,
             fontSize = viewModel.design.value.textSize.dp(),
