c6dac04248744d2e877383fe9c370d23bce54008
차지원 <chajiwon100785@gmail.com>
2021-11-30 12:01:21 +0900

    Issue #303 refactor: month에서 week를 반복호출하는 방식으로 변경

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/MonthCalendar.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/composeView/MonthCalendar.kt
@@ -1,25 +1,13 @@
 package com.drunkenboys.ckscalendar.yearcalendar.composeView
 
 import android.view.Gravity
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
 import com.drunkenboys.ckscalendar.data.*
-import com.drunkenboys.ckscalendar.utils.GravityMapper
 import com.drunkenboys.ckscalendar.utils.calendarSetToCalendarDatesList
 import com.drunkenboys.ckscalendar.yearcalendar.YearCalendarViewModel
 import java.time.LocalDate
@@ -31,89 +19,14 @@ fun MonthCalendar(
     dayColumnModifier: (CalendarDate) -> Modifier,
     viewModel: YearCalendarViewModel
 ) {
-    var weekSchedules: Array<Array<CalendarScheduleObject?>> // 1주 스케줄
-    val weekDayColor = MaterialTheme.colors.primary
-
     calendarSetToCalendarDatesList(month).forEach { week ->
-        weekSchedules = Array(7) { Array(viewModel.design.value.visibleScheduleCount) { null } }
-        // 1주일
-        ConstraintLayout(
-            constraintSet = dayOfWeekConstraints(week.map { day -> day.date.toString() }),
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            // 월 표시
-            if (viewModel.isFirstWeek(week, month)) AnimatedMonthHeader(listState = listState, monthName = month.name)
-
-            week.forEach { day ->
-                when (day.dayType) {
-                    // 빈 날짜
-                    DayType.PADDING -> Column(
-                        modifier = Modifier
-                            .layoutId(day.date.toString())
-                            .border(border = BorderStroke(width = 0.1f.dp, color = weekDayColor.copy(alpha = 0.1f)))
-                    ) {
-                        repeat(viewModel.design.value.visibleScheduleCount + 1) {
-                            Text(text = " ")
-                        }
-                    }
-
-                    // 1일
-                    else -> Column(
-                        modifier = Modifier.layoutId(day.date.toString())
-                            .then(dayColumnModifier(day))
-                            .drawBehind {
-                                drawLine(
-                                    start = Offset(0f, 0f),
-                                    end = Offset(size.width, 0f),
-                                    color = weekDayColor.copy(alpha = 0.1f)
-                                )
-                                drawLine(
-                                    start = Offset(size.width, 0f),
-                                    end = Offset(size.width, size.height),
-                                    color = weekDayColor.copy(alpha = 0.1f)
-                                )
-                                drawLine(
-                                    start = Offset(size.width, size.height),
-                                    end = Offset(0f, size.height),
-                                    color = weekDayColor.copy(alpha = 0.1f)
-                                )
-                            },
-                        horizontalAlignment = GravityMapper.toColumnAlign(viewModel.design.value.textAlign)
-                    ) {
-                        DayText(
-                            day = day,
-                            viewModel = viewModel,
-                            isFirstOfCalendarSet = month.id > 0 && (day.date.dayOfMonth == 1 || day.date == month.startDate)
-                        )
-
-                        ScheduleText(
-                            today = day.date,
-                            weekScheduleList = weekSchedules,
-                            viewModel = viewModel
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-private fun dayOfWeekConstraints(weekIds: List<String>) = ConstraintSet {
-    val week = weekIds.map { id ->
-        createRefFor(id)
-    }
-
-    week.forEachIndexed { i, ref ->
-        constrain(ref) {
-            width = Dimension.fillToConstraints
-            top.linkTo(parent.top)
-
-            if (i != 0) start.linkTo(week[i - 1].end)
-            else start.linkTo(parent.start)
-
-            if (i != week.size - 1) end.linkTo(week[i + 1].start)
-            else end.linkTo(parent.end)
-        }
+        WeekCalendar(
+            viewModel = viewModel,
+            listState = listState,
+            month = month,
+            dayColumnModifier = dayColumnModifier,
+            week = week
+        )
     }
 }
 
