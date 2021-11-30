9de49b017043d58e9aaa617718178d4a264a8b0f
simeunseok <ses8323@gmail.com>
2021-11-30 18:26:23 +0900

    Issue #281 feat: HolidayLocalDataSource 인터페이스 생성

---
+++ app/src/main/java/com/drunkenboys/calendarun/data/holiday/local/HolidayLocalDataSource.kt
@@ -0,0 +1,12 @@
+package com.drunkenboys.calendarun.data.holiday.local
+
+import com.drunkenboys.calendarun.data.holiday.entity.Holiday
+import kotlinx.coroutines.flow.Flow
+
+interface HolidayLocalDataSource {
+
+    suspend fun insertHoliday(holiday: Holiday)
+
+    fun fetchAllHoliday(): Flow<List<Holiday>>
+
+}
