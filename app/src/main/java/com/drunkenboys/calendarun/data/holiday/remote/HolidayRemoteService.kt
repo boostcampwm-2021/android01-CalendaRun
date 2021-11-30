32ad46a0249e68c15d96de8e55fe946d177af1f4
simeunseok <ses8323@gmail.com>
2021-12-01 00:22:50 +0900

    Issue #281 feat: 단일 Item Response를 반환하는 Retrofit

--- app/src/main/java/com/drunkenboys/calendarun/data/holiday/remote/HolidayRemoteService.kt
+++ app/src/main/java/com/drunkenboys/calendarun/data/holiday/remote/HolidayRemoteService.kt
@@ -2,10 +2,26 @@ package com.drunkenboys.calendarun.data.holiday.remote
 
 import com.drunkenboys.calendarun.BuildConfig
 import com.drunkenboys.calendarun.data.holiday.ResponseHolidayInfo
+import com.drunkenboys.calendarun.data.holiday.ResponseHolidayListInfo
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface HolidayRemoteService {
+
+    @GET("/B090041/openapi/service/SpcdeInfoService/getRestDeInfo")
+    suspend fun fetchHolidayListOnYear(
+        @Query("solYear") year: String,
+        @Query("_type") type: String = "json",
+        @Query("ServiceKey") serviceKey: String = BuildConfig.HOLIDAY_API_KEY
+    ): ResponseHolidayListInfo
+
+    @GET("/B090041/openapi/service/SpcdeInfoService/getRestDeInfo")
+    suspend fun fetchHolidayListOnMonth(
+        @Query("solYear") year: String,
+        @Query("solMonth") month: String,
+        @Query("_type") type: String = "json",
+        @Query("ServiceKey") serviceKey: String = BuildConfig.HOLIDAY_API_KEY
+    ): ResponseHolidayListInfo
 
     @GET("/B090041/openapi/service/SpcdeInfoService/getRestDeInfo")
     suspend fun fetchHolidayOnYear(
