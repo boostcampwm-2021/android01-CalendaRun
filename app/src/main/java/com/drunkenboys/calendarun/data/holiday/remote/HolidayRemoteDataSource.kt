a7c7e74e1b9d0e5ef5de7e0efed1235d35443876
simeunseok <ses8323@gmail.com>
2021-12-01 00:23:34 +0900

    Issue #281 feat: 단일 Response fetch 인터페이스 추가

--- app/src/main/java/com/drunkenboys/calendarun/data/holiday/remote/HolidayRemoteDataSource.kt
+++ app/src/main/java/com/drunkenboys/calendarun/data/holiday/remote/HolidayRemoteDataSource.kt
@@ -1,8 +1,13 @@
 package com.drunkenboys.calendarun.data.holiday.remote
 
 import com.drunkenboys.calendarun.data.holiday.ResponseHolidayInfo
+import com.drunkenboys.calendarun.data.holiday.ResponseHolidayListInfo
 
 interface HolidayRemoteDataSource {
+
+    suspend fun fetchHolidayListOnYear(year: String): ResponseHolidayListInfo
+
+    suspend fun fetchHolidayListOnMonth(year: String, month: String): ResponseHolidayListInfo
 
     suspend fun fetchHolidayOnYear(year: String): ResponseHolidayInfo
 
