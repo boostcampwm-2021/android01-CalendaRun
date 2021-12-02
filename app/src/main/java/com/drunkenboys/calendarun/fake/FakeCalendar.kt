fb329cdfa4cc1cdffaa70b66dc90b922c821ff83
차지원 <chajiwon100785@gmail.com>
2021-12-02 14:35:48 +0900

    Issue #355 fix: calendar Id를 1로 변환

--- app/src/main/java/com/drunkenboys/calendarun/fake/FakeCalendar.kt
+++ app/src/main/java/com/drunkenboys/calendarun/fake/FakeCalendar.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 
 // 부스트캠프 그룹 프로젝트 + 네트워킹 데이의 기간이 포함된 달력
 fun getFakeCalendar() = Calendar(
-    id = -1L,
+    id = 1L,
     name = "",
     startDate = LocalDate.of(2021, 10, 25),
     endDate = LocalDate.of(2021, 12, 7)
