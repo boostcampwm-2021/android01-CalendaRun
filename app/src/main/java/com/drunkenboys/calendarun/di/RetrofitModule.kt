d4386e55973d26f4a3e1272ab67de93667d7d44a
simeunseok <ses8323@gmail.com>
2021-12-02 14:43:49 +0900

    Issue #345 feat: Gson setLenient 추가

--- app/src/main/java/com/drunkenboys/calendarun/di/RetrofitModule.kt
+++ app/src/main/java/com/drunkenboys/calendarun/di/RetrofitModule.kt
@@ -1,5 +1,6 @@
 package com.drunkenboys.calendarun.di
 
+import com.google.gson.GsonBuilder
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -14,12 +15,16 @@ object RetrofitModule {
 
     private const val HOLIDAY_URL = "http://apis.data.go.kr"
 
+    private val gson = GsonBuilder()
+        .setLenient()
+        .create()
+
     @Provides
     @Singleton
     fun provideHolidayRetrofit(): Retrofit =
         Retrofit.Builder()
             .baseUrl(HOLIDAY_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(gson))
             .build()
 
 }
