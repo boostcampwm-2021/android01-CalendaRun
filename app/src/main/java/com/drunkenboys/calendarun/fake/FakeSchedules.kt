e6ffa9f22d2a3a054c7a977a2a89bfc9dbec5c12
차지원 <chajiwon100785@gmail.com>
2021-12-02 14:58:25 +0900

    Issue #355 fix: id 고유하게 변환

--- app/src/main/java/com/drunkenboys/calendarun/fake/FakeSchedules.kt
+++ app/src/main/java/com/drunkenboys/calendarun/fake/FakeSchedules.kt
@@ -4,10 +4,12 @@ import android.graphics.Color
 import com.drunkenboys.calendarun.data.schedule.entity.Schedule
 import java.time.LocalDateTime
 
+private var id = 20211202L
+
 fun getFakeSchedules() = listOf(
     Schedule(
-        id = 0,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "프로젝트 기획",
         startDate = LocalDateTime.of(2021, 10, 25, 0, 0),
         endDate = LocalDateTime.of(2021, 10, 27, 23, 59),
@@ -17,8 +19,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 1,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "프로토타입 작성",
         startDate = LocalDateTime.of(2021, 10, 27, 0, 0),
         endDate = LocalDateTime.of(2021, 10, 28, 23, 59),
@@ -28,8 +30,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 2,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "라이브러리 설정",
         startDate = LocalDateTime.of(2021, 10, 28, 0, 0),
         endDate = LocalDateTime.of(2021, 10, 28, 23, 59),
@@ -39,8 +41,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 3,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "역할 분배 및 문서 작업",
         startDate = LocalDateTime.of(2021, 10, 29, 0, 0),
         endDate = LocalDateTime.of(2021, 10, 29, 23, 59),
@@ -51,8 +53,8 @@ fun getFakeSchedules() = listOf(
 
     // ============================2주차 ======================================================
     Schedule(
-        id = 4,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "데이터 설계",
         startDate = LocalDateTime.of(2021, 11, 1, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 1, 23, 59),
@@ -62,8 +64,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 5,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "인터페이스 결정",
         startDate = LocalDateTime.of(2021, 11, 1, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 1, 23, 59),
@@ -73,8 +75,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 6,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "달력 디자인 요소 정리",
         startDate = LocalDateTime.of(2021, 11, 2, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 3, 23, 59),
@@ -84,8 +86,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 7,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "달력 레이아웃 방식 실험",
         startDate = LocalDateTime.of(2021, 11, 2, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 3, 23, 59),
@@ -95,8 +97,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 11,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "DB 구현",
         startDate = LocalDateTime.of(2021, 11, 2, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 2, 23, 59),
@@ -106,8 +108,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 12,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "일정 CRUD",
         startDate = LocalDateTime.of(2021, 11, 3, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 4, 23, 59),
@@ -117,8 +119,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 13,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "일정 검색",
         startDate = LocalDateTime.of(2021, 11, 4, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 5, 23, 59),
@@ -128,8 +130,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 14,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "메인화면 기능구현",
         startDate = LocalDateTime.of(2021, 11, 2, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 4, 23, 59),
@@ -139,8 +141,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 15,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "일정 알림 기능",
         startDate = LocalDateTime.of(2021, 11, 3, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 4, 23, 59),
@@ -151,8 +153,8 @@ fun getFakeSchedules() = listOf(
 
     // ======================3주차 ===============================
     Schedule(
-        id = 8,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "월달력 구현",
         startDate = LocalDateTime.of(2021, 11, 4, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 10, 23, 59),
@@ -162,8 +164,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 9,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "연달력 구현",
         startDate = LocalDateTime.of(2021, 11, 4, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 10, 23, 59),
@@ -173,8 +175,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 10,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "라이브러리 연동",
         startDate = LocalDateTime.of(2021, 11, 8, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 11, 23, 59),
@@ -184,8 +186,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 22,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "Flow로 마이그레이션",
         startDate = LocalDateTime.of(2021, 11, 8, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 11, 23, 59),
@@ -197,8 +199,8 @@ fun getFakeSchedules() = listOf(
 
     // ===============================4주차 =========================
     Schedule(
-        id = 16,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "휴가",
         startDate = LocalDateTime.of(2021, 11, 15, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 15, 23, 59),
@@ -208,8 +210,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 15,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "월달력 성능 개선",
         startDate = LocalDateTime.of(2021, 11, 16, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 18, 23, 59),
@@ -220,8 +222,8 @@ fun getFakeSchedules() = listOf(
 
 
     Schedule(
-        id = 17,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "연달력 성능 개선",
         startDate = LocalDateTime.of(2021, 11, 16, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 18, 23, 59),
@@ -231,8 +233,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 18,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "일정 페이징 기능",
         startDate = LocalDateTime.of(2021, 11, 16, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 18, 23, 59),
@@ -243,8 +245,8 @@ fun getFakeSchedules() = listOf(
 
     // =================================5주차 ======================
     Schedule(
-        id = 19,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "위젯 기능 추가",
         startDate = LocalDateTime.of(2021, 11, 22, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 25, 23, 59),
@@ -254,8 +256,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 20,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "QA",
         startDate = LocalDateTime.of(2021, 11, 24, 0, 0),
         endDate = LocalDateTime.of(2021, 11, 25, 23, 59),
@@ -266,8 +268,8 @@ fun getFakeSchedules() = listOf(
 
     // ===========================6주차===============================
     Schedule(
-        id = 21,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "공휴일 지원",
         startDate = LocalDateTime.of(2021, 11, 29, 0, 0),
         endDate = LocalDateTime.of(2021, 12, 1, 23, 59),
@@ -277,8 +279,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 23,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "테스트",
         startDate = LocalDateTime.of(2021, 11, 29, 0, 0),
         endDate = LocalDateTime.of(2021, 12, 2, 23, 59),
@@ -288,8 +290,8 @@ fun getFakeSchedules() = listOf(
     ),
 
     Schedule(
-        id = 24,
-        calendarId = 1L,
+        id = id++,
+        calendarId = 20211202L,
         name = "디버깅",
         startDate = LocalDateTime.of(2021, 11, 29, 0, 0),
         endDate = LocalDateTime.of(2021, 12, 2, 23, 59),
