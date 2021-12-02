d07de1493e63db28823eab22bf6e7e7cb2e5f101
Seonghwan Kim <shshksh58@gmail.com>
2021-12-02 17:09:42 +0900

    test

--- app/src/main/java/com/drunkenboys/calendarun/view/Test.kt
+++ app/src/main/java/com/drunkenboys/calendarun/view/Test.kt
@@ -1,23 +1,5 @@
 package com.drunkenboys.calendarun.view
 
 class Test {
-    class Test {
 
-    }
-
-    class Test2 {
-
-    }
-
-    class Test3 {
-
-    }
-
-    class Test4 {
-
-    }
-
-    class Test5 {
-
-    }
 }
