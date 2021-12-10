c1d2c9a9b2df1fd9a4ec8875b4837cdec6eb8122
simeunseok <ses8323@gmail.com>
2021-12-10 21:57:30 +0900

    Issue #377 feat: Flow로 반응형 위젯 리스트 구현

--- app/src/main/java/com/drunkenboys/calendarun/CalendaRunRemoteViewsFactory.kt
+++ app/src/main/java/com/drunkenboys/calendarun/CalendaRunRemoteViewsFactory.kt
@@ -4,67 +4,53 @@ import android.content.Context
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
 import com.drunkenboys.calendarun.data.schedule.local.ScheduleLocalDataSource
-import com.drunkenboys.calendarun.ui.searchschedule.model.ScheduleItem
+import com.drunkenboys.calendarun.util.getEndOfDate
+import com.drunkenboys.calendarun.util.getStartOfDate
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
-import java.time.LocalDateTime
 
 class CalendaRunRemoteViewsFactory constructor(
     private val context: Context,
     private val scheduleDataSource: ScheduleLocalDataSource
 ) : RemoteViewsService.RemoteViewsFactory {
 
-    private val job = SupervisorJob()
-    private val coroutineScope = CoroutineScope(Dispatchers.IO + job)
-
-    private var scheduleList: List<ScheduleItem> = listOf()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    private var scheduleList = scheduleDataSource.fetchDateSchedule(
+        getStartOfDate(), getEndOfDate()
+    ).stateIn(coroutineScope, SharingStarted.Eagerly, emptyList())
 
     override fun onCreate() {
-        setDate()
-    }
-
-    override fun onDataSetChanged() {
-        setDate()
-    }
-
-    private fun setDate() {
         coroutineScope.launch {
-            scheduleDataSource.fetchDateSchedule(LocalDateTime.now()).collectLatest { newScheduleList ->
-                scheduleList = newScheduleList.map { schedule ->
-                    ScheduleItem(schedule) {}
-                }
+            scheduleList.collectLatest {
+                CalendaRunAppWidget.sendUpdateBroadcast(context)
             }
         }
     }
 
+    override fun onDataSetChanged() {}
+
     override fun onDestroy() {}
 
-    override fun getCount() = scheduleList.size
+    override fun getCount() = scheduleList.value.size
 
     override fun getViewAt(position: Int): RemoteViews {
         val widget = RemoteViews(context.packageName, R.layout.item_app_widget_schedule).apply {
-            setTextViewText(R.id.tv_appWidget_schedule_name, scheduleList[position].schedule.name)
-            setTextViewText(R.id.tv_appWidget_schedule_memo, scheduleList[position].schedule.memo)
+            setTextViewText(R.id.tv_appWidget_schedule_name, scheduleList.value[position].name)
+            setTextViewText(R.id.tv_appWidget_schedule_memo, scheduleList.value[position].memo)
         }
 
         return widget
     }
 
-    override fun getLoadingView(): RemoteViews? {
-        // TODO: 2021-11-29 로딩 뷰 추가
-        return null
-    }
+    override fun getLoadingView(): RemoteViews? = null
 
-    override fun getViewTypeCount() = VIEW_TYPE_COUNT
+    override fun getViewTypeCount() = 1
 
     override fun getItemId(position: Int) = 0L
 
     override fun hasStableIds() = false
-
-    companion object {
-        private const val VIEW_TYPE_COUNT = 1
-    }
 }
