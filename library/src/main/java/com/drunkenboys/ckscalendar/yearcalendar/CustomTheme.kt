b7110d83c6f7a1de0e17e14531529926104e208b
차지원 <chajiwon100785@gmail.com>
2021-12-20 21:24:11 +0900

    Issue #385 chore: Custom Theme 주석 추가

--- library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/CustomTheme.kt
+++ library/src/main/java/com/drunkenboys/ckscalendar/yearcalendar/CustomTheme.kt
@@ -6,6 +6,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import com.drunkenboys.ckscalendar.data.CalendarDesignObject
 
+/**
+ * FIXME: View에서 Theme과 design의 color를 혼용하고 있음.
+ * Theme
+ */
 @Composable
 fun CustomTheme(
     design: CalendarDesignObject,
@@ -19,6 +23,8 @@ fun CustomTheme(
         background = Color(design.backgroundColor),
     )
 
+    // 존재하는 MaterialTheme에서 color set을 커스텀.
+    // View를 선언할 때 MaterialTheme로 감싸면 테마를 적용할 수 있다.
     MaterialTheme(
         colors = lightColors,
         content = content
